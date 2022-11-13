a361d80b2a21cc2c7ee381c27853d87141418788
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-14 00:59:13 +0900

    🚧 Change mock data to real data and add Loading component when isLoading is true

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,27 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import FakeYoutube from '../api/mockYoutube';
+import Youtube from '../api/youtube';
+import Loading from '../components/Loading';
 import VideoCard from '../components/VideoCard';
 import { IVideo } from '../interface';
 
 export default function Videos() {
-  const youtube = new FakeYoutube();
+  const youtube = new Youtube();
 
   const { keyword } = useParams();
 
-  const {
-    isLoading,
-    error,
-    data: videos,
-  } = useQuery(['videos', keyword], () =>
-    youtube.search(keyword).then((res) => res.data.items)
+  const { isLoading, data: videos } = useQuery(
+    ['videos', keyword],
+    () => youtube.videos(keyword),
+    {
+      staleTime: 1000 * 60 * 5,
+    }
   );
 
   return (
     <section className="overflow-hidden">
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Something is wrong</p>}
+      {isLoading && <Loading />}
       {videos && (
         <ul className="p-5 lg:px-16 mt-14 md:mt-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-16">
           {videos.map((video: IVideo) => (
