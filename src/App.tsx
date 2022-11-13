00b3c419fd2d06b3e2a0dfa107b3ffb4990ff330
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-14 03:43:45 +0900

    🚧 Mark up and styled SideNav component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SideNav from './components/SideNav';
 
 const client = new QueryClient();
 
@@ -12,6 +13,7 @@ function App() {
       <QueryClientProvider client={client}>
         <Outlet />
       </QueryClientProvider>
+      <SideNav />
     </>
   );
 }
