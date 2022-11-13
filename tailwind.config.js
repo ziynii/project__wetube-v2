010a937aadb227071b4346f8ab060820a78eea56
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-14 00:53:41 +0900

    💄 Add custom-style for loading animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,34 @@ module.exports = {
       colors: {
         brand: '#FF0000',
       },
-			padding: {
-				'56': '56.25%'
-			}
+      padding: {
+        56: '56.25%',
+      },
+      keyframes: {
+        loading: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        loading: 'loading 1.6s linear infinite',
+      },
+      animationDelay: {
+        48: '0.48s',
+        60: '0.6s',
+        72: '0.72s',
+        84: '0.84s',
+        96: '0.96s',
+        108: '1.08s',
+        120: '1.2s',
+        132: '1.32s',
+        144: '1.44s',
+        156: '1.56s',
+      },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('tailwindcss-animation-delay'),
+  ],
 };
