25bca3bafcf4f7d5ad8d350b94106c9e8bf973a7
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-14 01:38:43 +0900

    🚧 Implement NotFound component

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className='font-mono pr-2 animate-cursor'>404, Page not found.</h1>
+    </div>
+  );
+}
