72446c092b48d4e8f458a92e877f1f0387240d27
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-14 02:11:11 +0900

    🚧 if openSearch is true, SearchInput is shown when not on desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../image/logo.png';
 import { HiBars3CenterLeft } from 'react-icons/hi2';
 import { IoPersonCircle } from 'react-icons/io5';
 import { BiSearch } from 'react-icons/bi';
 import { VscBell } from 'react-icons/vsc';
+import { BiX } from 'react-icons/bi';
 import SearchInput from './SearchInput';
 
 export default function Header() {
+  const [openSearch, setOpenSearch] = useState(false);
   return (
     <header className="fixed w-full flex items-center py-5 px-5 justify-between bg-white border-b border-gray-200 z-50 lg:px-16">
       <div className="basis-4/5 md:basis-1/5 flex">
@@ -32,6 +34,18 @@ export default function Header() {
         <SearchInput />
       </div>
 
+      {openSearch && (
+        <div className="flex items-center absolute  w-full bg-white">
+          <button
+            className="text-2xl text-zinc-600"
+            onClick={() => setOpenSearch(false)}
+          >
+            <BiX />
+          </button>
+          <SearchInput />
+        </div>
+      )}
+
       <div className="basis-1/5 flex justify-end">
         <div className="hidden items-center text-2xl text-zinc-600 md:flex">
           <button className="mr-3">
@@ -44,7 +58,10 @@ export default function Header() {
         <button className="hidden w-24 py-2 ml-3 bg-brand text-white rounded-md lg:block">
           Login
         </button>
-        <button className="text-2xl text-zinc-600 md:hidden">
+        <button
+          className="text-2xl text-zinc-600 md:hidden"
+          onClick={() => setOpenSearch(true)}
+        >
           <BiSearch />
         </button>
       </div>
