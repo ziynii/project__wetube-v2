369d03c0c5aff04e3605bf21dc6dce6dd786d626
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-17 02:41:41 +0900

    🚚 Chane to .ts file

--- src/util/date.ts
+++ src/util/date.ts
@@ -3,6 +3,6 @@ import koLocale from 'timeago.js/lib/lang/ko';
 
 register('ko', koLocale);
 
-export function formatAgo(date, lang = 'en_US') {
+export function formatAgo(date: string, lang = 'en_US') {
   return format(date, lang);
 }
