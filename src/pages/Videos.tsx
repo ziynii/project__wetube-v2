62aa5363026d4bdb980e8b2d95c4abbf3f1966da
Jiyeon <ziyeon.ivi@gmail.com>
2022-11-17 02:37:37 +0900

    💄 Styled section and ul when open sideNav

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import Youtube from '../api/youtube';
 import Loading from '../components/Loading';
 import VideoCard from '../components/VideoCard';
+import { OpenSideNavContext } from '../context/OpenSideNavContext';
 import { IVideo } from '../interface';
 
 export default function Videos() {
   const youtube = new Youtube();
+  const { openSideNav } = useContext(OpenSideNavContext);
 
   const { keyword } = useParams();
 
@@ -20,10 +22,15 @@ export default function Videos() {
   );
 
   return (
-    <section className="overflow-hidden dark:bg-slate-800 dark:text-white">
+    <section className="overflow-hidden lg:flex lg:justify-end dark:bg-slate-800 dark:text-white">
       {isLoading && <Loading />}
       {videos && (
-        <ul className="p-5 lg:px-16 mt-14 md:mt-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-12">
+        <ul
+          className={
+            'p-5 lg:px-16 mt-14 md:mt-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-12 ' +
+            (openSideNav ? 'lg:w-isSideNav' : '')
+          }
+        >
           {videos.map((video: IVideo) => (
             <VideoCard key={video.id} video={video} type="list" />
           ))}
